c0a817643a93b7323b93d93844148408578c8932
dale <homelala@naver.com>
2024-10-30 11:20:54 +0900

    효율성 - 5. 연속된 자연수 합

--- algorithm/src/efficiency/ContinuousElementSum.java
+++ algorithm/src/efficiency/ContinuousElementSum.java
@@ -48,11 +48,42 @@ public class ContinuousElementSum {
 
         return result;
     }
+    public static int solution3(int n) {
+        int result = 0;
+
+        for (int i = 2; i < n / 2 + 1; i++) {
+            int sum = 0;
+            for (int j = 1; j <= i;j++) {
+                sum += j;
+            }
+            if (sum <=n && (n - sum) % i == 0) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public static int solution4(int n) {
+        int result = 0, len = 1;
+        n--;
+
+        while (n > 0) {
+            len++;
+            n -= len;
+            if (n % len == 0) {
+                result++;
+            }
+
+        }
+
+        return result;
+    }
 
     public void main() {
         Scanner in=new Scanner(System.in);
         int n = in.nextInt();
 
-        System.out.print(solution2(n));
+        System.out.print(solution4(n));
     }
 }
