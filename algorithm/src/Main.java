17979d8ab24e4b99541c6abe90dd8c81dfcdb2d7
dale <homelala@naver.com>
2024-11-11 11:19:52 +0900

    sort - 6. 장난꾸러기

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,7 +2,7 @@ import sort.*;
 
 public class Main {
     public static void main(String[] args) {
-        CommonCheck solution = new CommonCheck();
+        Naughty solution = new Naughty();
         solution.main();
     }
 }
---
+++ algorithm/src/sort/Naughty.java
@@ -0,0 +1,39 @@
+package sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 다시 풀어보기
+public class Naughty {
+
+    public static ArrayList<Integer> solution1(int n, int[] inputArray) {
+        ArrayList<Integer> result = new ArrayList<>();
+        int a=0, b=n;
+        int[] temp = inputArray.clone();
+        Arrays.sort(temp);
+
+        for (int i = 0; i < n; i++) {
+            if (temp[i] != inputArray[i]) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+
+
+
+    public void main() {
+        Scanner in=new Scanner(System.in);
+        int n = in.nextInt();
+        int[] inputArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            inputArray[i] = in.nextInt();
+        }
+
+        for (int i : solution1(n, inputArray)) {
+            System.out.printf(i + " ");
+        }
+
+    }
+}
