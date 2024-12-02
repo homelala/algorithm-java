a975e773c69bbc35f74c5384622a6d6bf2970288
dale <homelala@naver.com>
2024-12-02 12:06:59 +0900

    [탐욕] 2. 회의실 배정

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,4 +1,5 @@
 import DFSBFS.*;
+import Greedy.*;
 import Recursion.*;
 import sort.BinarySort2;
 import sort.LRU2;
@@ -6,7 +7,7 @@ import sort.MusicVideo2;
 
 public class Main {
     public static void main(String[] args) {
-        Pizza solution = new Pizza();
+        Cabinet solution = new Cabinet();
         solution.main();
     }
 }
