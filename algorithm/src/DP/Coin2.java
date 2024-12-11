133c331610641584b11e0e14196e6a27be5010d0
dale <homelala@naver.com>
2024-12-11 10:39:25 +0900

    [DP] 다시 풀어보기

---
+++ algorithm/src/DP/Coin2.java
@@ -0,0 +1,44 @@
+package DP;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//다시 풀어보기
+public class Coin2 {
+    static int[] dp;
+    static int[] coins;
+
+    public static int solution1(int n, int m) {
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = coins[i]; j <= m; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+
+        return dp[m];
+    }
+
+
+    public void main() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        coins = new int[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        dp = new int[m + 1];
+
+        bw.write(solution1(n, m)+"");
+        bw.flush();
+        bw.close();
+    }
+}
--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        MakePassword solution = new MakePassword();
+        Coin2 solution = new Coin2();
         solution.main();
     }
 }
