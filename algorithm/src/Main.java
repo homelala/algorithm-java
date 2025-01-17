a8a4612478ef54705208d44bb78a2c1c202541ac
dale <homelala@naver.com>
2025-01-17 16:18:31 +0900

    [탐욕] 백준 다시 풀기

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Wine solution = new Wine();
+        ClassRoom solution = new ClassRoom();
         solution.main();
     }
 }
